10db6624d455834cff2af0fafeebe8dff28b2a6a
demiusdelph-developer <53659364+demiusdelph-developer@users.noreply.github.com>
2019-12-02 11:20:14 +0200

    Установка gulp, создание тасков browser-sync и minify-css

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+let gulp = require('gulp'),
+    cleanCSS = require('gulp-clean-css'),
+    rename = require('gulp-rename'),
+    browserSync = require('browser-sync');
+
+  gulp.task('minify-css', () => {
+    return gulp.src('css/*.css')
+      .pipe(cleanCSS({compatibility: 'ie8'}))
+      .pipe(rename({
+        suffix: '.min',
+        prefix: ''
+      }))
+      .pipe(gulp.dest('css'));
+  });
