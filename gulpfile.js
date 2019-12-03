799f14e9e932a165e743a50bc159e4b824b271ce
demiusdelph-developer <53659364+demiusdelph-developer@users.noreply.github.com>
2019-12-03 08:34:24 +0200

    изменения в gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
-let gulp = require('gulp'),
+var gulp = require('gulp'),
     cleanCSS = require('gulp-clean-css'),
     rename = require('gulp-rename'),
     browserSync = require('browser-sync');
 
-  gulp.task('minify-css', () => {
-    return gulp.src('css/*.css')
+  gulp.task('minify-css', function() {
+    return gulp.src(['css/*.css', '!css/*.min.css'])
       .pipe(cleanCSS({compatibility: 'ie8'}))
       .pipe(rename({
         suffix: '.min',
@@ -13,7 +13,7 @@ let gulp = require('gulp'),
       .pipe(gulp.dest('css'));
   });
 
-  gulp.task('browser-sync', () => {
+  gulp.task('browser-sync', function(){
     browserSync.init({
       server: {
         baseDir: './'
