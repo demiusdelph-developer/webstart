d42dfd2fb1b73996eedb9a035a28cf97ea465734
demiusdelph-developer <53659364+demiusdelph-developer@users.noreply.github.com>
2019-12-05 18:12:42 +0200

    feat: enables sass module and create sass files

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,41 @@
-var gulp = require('gulp'),
-    cleanCSS = require('gulp-clean-css'),
-    rename = require('gulp-rename'),
-    browserSync = require('browser-sync');
+const {src,dest,watch} = require('gulp'),
+      cleanCSS = require('gulp-clean-css'),
+      rename = require('gulp-rename'),
+      browserSync = require('browser-sync').create(),
+      sass = require('gulp-sass');
 
-  gulp.task('minify-css', function() {
-    return gulp.src(['css/*.css', '!css/*.min.css'])
-      .pipe(cleanCSS({compatibility: 'ie8'}))
-      .pipe(rename({
-        suffix: '.min',
-        prefix: ''
-      }))
-      .pipe(gulp.dest('css'));
-  });
+  // function minifyCSS() {
+  //   return src(['css/*.css', '!css/*.min.css'])
+  //     .pipe(cleanCSS({compatibility: 'ie8'}))
+  //     .pipe(rename({
+  //       suffix: '.min',
+  //       prefix: ''
+  //     }))
+  //     .pipe(dest('css'));
+  // }
 
-  gulp.task('browser-sync', function(){
+  function bs(){
+    serveSass();
     browserSync.init({
       server: {
         baseDir: './'
       }
     });
-  });
+    watch("./*.html").on('change', browserSync.reload);
+    watch("./sass/**/*.sass", serveSass);
+    watch("./*.js").on('change', browserSync.reload);
+  }
+
+  function serveSass() {
+    return src("./sass/*.sass")
+        .pipe(sass())
+        .pipe(rename({
+          suffix: '.min',
+          prefix: ''
+        }))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(dest('./css'))
+        .pipe(browserSync.stream());
+}
+
+exports.serve = bs;
