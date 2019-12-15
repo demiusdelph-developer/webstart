362ab3e3e18b37d4ca8c46f57699814c25c0a492
demiusdelph-developer <53659364+demiusdelph-developer@users.noreply.github.com>
2019-12-15 06:36:41 +0200

    fix: fonts and paddings

--- js/main.js
+++ js/main.js
@@ -1,13 +1,39 @@
+// window.addEventListener('DOMContentLoaded', () => {
+//   'use strict';
+
+//   const modal = document.querySelector('.modal'),
+//         modalBtn = document.querySelectorAll('[data-toggle=modal]'),
+//         closeBtn = document.querySelector('.modal__close'),
+//         switchModal = () => {
+//           modal.classList.toggle('modal--visible')
+//         }
+//   modalBtn.forEach(i => {
+//     i.addEventListener('click', switchModal);
+//   });
+//   modal.addEventListener('click', (e) => {
+//     if(!(e.target.closest('.modal__dialog'))) switchModal();
+//   });
+//   closeBtn.addEventListener('click', switchModal);
+// });
+
+
+
 $(document).ready(function () {
   var modal = $('.modal'),
+      modalDialog = $('.modal__dialog'),
       modalBtn = $('[data-toggle="modal"]'),
       closeBtn = $('.modal__close');
 
   modalBtn.on('click', function() {
     modal.toggleClass('modal--visible');
   });
+  modal.on('click', function(e) {
+    if(!modalDialog.is(e.target) && modalDialog.has(e.target).length === 0) {
+      modal.toggleClass('modal--visible');
+    }
+  });
   closeBtn.on('click', function() {
     modal.toggleClass('modal--visible');
-  })
+  });
 });
 
